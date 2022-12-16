8c3d05a331dca2b802b7c41757b602fb9b998d2e
Galliss13 <matheusgalindo1313@gmail.com>
2022-12-16 16:21:30 -0300

    exercício 4 JOIN

---
+++ ExercíciosJOIN/04.sql
@@ -0,0 +1,1 @@
+SELECT users.id, users.name, roles.name AS "role", companies.name AS company, experiences."startDate" FROM experiences JOIN roles ON experiences."roleId"=roles.id JOIN companies ON experiences."companyId"=companies.id JOIN users ON experiences."userId"=users.id WHERE experiences."userId"=50 AND experiences."endDate" IS NULL;
