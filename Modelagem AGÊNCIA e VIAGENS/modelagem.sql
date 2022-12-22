7b13cf73fd14371150f3337eb08513f44cbc6752
Galliss13 <matheusgalindo1313@gmail.com>
2022-12-22 09:42:23 -0300

    Modelagem agências e voos

---
+++ Modelagem AGÊNCIA e VIAGENS/modelagem.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "companies" (
+	"id" SERIAL PRIMARY KEY,
+	"abbreviation" TEXT NOT NULL,
+	"name" TEXT NOT NULL
+)
+
+CREATE TABLE "flights" (
+	"id" SERIAL PRIMARY KEY,
+	"abbreviation" TEXT NOT NULL,
+	"companyId" INTEGER NOT NULL REFERENCES "companies"("id"),
+	"departureId" INTEGER NOT NULL REFERENCES "airports"("id"),
+	"destinationId" INTEGER NOT NULL REFERENCES "airports"("id"),
+	"departureTime" TIMESTAMP NOT NULL,
+	"arrivalTime" TIMESTAMP NOT NULL
+)
+
+CREATE TABLE "airports" (
+	"id" SERIAL PRIMARY KEY,
+	"abbreviation" TEXT NOT NULL,
+	"name" TEXT NOT NULL
+)
